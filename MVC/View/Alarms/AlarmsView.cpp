2064c3a244283e49ceab39f7a4f5e398358466bb
Wojciech <wojciechkraw99@gmail.com>
2026-01-30 15:50:21 +0100

    Update

--- MVC/View/Alarms/AlarmsView.cpp
+++ MVC/View/Alarms/AlarmsView.cpp
@@ -8,6 +8,18 @@ AlarmsView::AlarmsView(QWidget *parent) :
 
     setWindowFlags(windowFlags() | Qt::WindowStaysOnTopHint);
 
+    connect(ui->lowHealth_checkBox, &QCheckBox::clicked, this, [this](bool checked){
+        ui->lowHealth_lineEdit->setEnabled(!checked);
+    });
+
+    connect(ui->lowMana_checkBox, &QCheckBox::clicked, this, [this](bool checked){
+        ui->lowMana_lineEdit->setEnabled(!checked);
+    });
+
+    connect(ui->creatureDetected_checkBox, &QCheckBox::clicked, this, [this](bool checked){
+        ui->groupBox->setEnabled(!checked);
+    });
+
 }
 
 AlarmsView::~AlarmsView() {
